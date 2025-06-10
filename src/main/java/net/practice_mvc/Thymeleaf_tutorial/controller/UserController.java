b08deca4158ae6f8867d90dc122738d25146305c
Anushree <anushreebhatt357@gmail.com>
2025-06-10 13:27:43 -0400

    variable expressions - .

---
+++ src/main/java/net/practice_mvc/Thymeleaf_tutorial/controller/UserController.java
@@ -0,0 +1,26 @@
+package net.practice_mvc.Thymeleaf_tutorial.controller;
+
+import net.practice_mvc.Thymeleaf_tutorial.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/variable-expressions")
+    public String variableExpression(Model model){
+        User user1 = new User("Anushree", "anushreebhatt357@gmail.com", "ADMIN", "female");
+        List<User> users = Arrays.asList(
+                user1,
+                new User("Ramesh", "ramesh@gmail.com", "USER", "male"),
+                new User("Ananya", "ananya@gmail.com", "USER", "female")
+        );
+        model.addAttribute("user", user1);
+        model.addAttribute("users", users);
+        return "variable_expressions";
+    }
+}
